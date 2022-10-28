435e7a1c6706d6971d77f14f8a41839a297fee9a
Dong Jackie <jd2598@zoo.cs.yale.edu>
2022-10-28 18:00:17 -0400

    bounded_buffer progress

--- kern/lib/bounded_buffer.c
+++ kern/lib/bounded_buffer.c
@@ -1,17 +1,55 @@
 #include "bounded_buffer.h"
+#include "spinlock.h"
+#include "cv.h"
 
-struct _bounded_buffer_t {
+#define BUFFER_SIZE 5
 
+struct _bounded_buffer_t
+{
+    spinlock_t lock;
+    cv_t item_added;
+    cv_t item_removed;
+    unsigned int items[BUFFER_SIZE];
+    unsigned int front;
+    unsigned int next_empty;
 };
 
-void bbq_init(bounded_buffer_t *bbq) {
-
+void bbq_init(bounded_buffer_t *bbq)
+{
+    bbq->front = 0;
+    bbq->next_empty = 0;
+    spinlock_init(&bbq->lock);
+    cv_init(&bbq->item_added);
+    cv_init(&bbq->item_removed);
 }
 
-void bbq_insert(bounded_buffer_t *bbq, unsigned int value) {
+// inserts an item with value into the bounded buffer queue
+void bbq_insert(bounded_buffer_t *bbq, unsigned int value)
+{
+    spinlock_acquire(&bbq->lock);
+    while ((bbq->next_empty - bbq->front) == BUFFER_SIZE)
+    {
+        cv_wait(&bbq->item_removed, &bbq->lock);
+    }
 
+    bbq->items[bbq->next_empty % BUFFER_SIZE] = value;
+    bbq->next_empty++;
+    cv_signal(&bbq->item_removed);
+    spinlock_release(&bbq->lock);
 }
 
-unsigned int bbq_remove(bounded_buffer_t *bbq) {
+// removes an item from the bounded buffer queue and returns it
+unsigned int bbq_remove(bounded_buffer_t *bbq)
+{
 
+    spinlock_acquire(&bbq->lock);
+    while (bbq->front == bbq->next_empty)
+    {
+        cv_wait(&bbq->item_added, &bbq->lock);
+    }
+    item = bbq->items[bbq->front % BUFFER_SIZE];
+    bbq->front++;
+    cv_signal(&bbq->item_added);
+    spinlock_release(&bbq->lock);
+    return item;
 }
