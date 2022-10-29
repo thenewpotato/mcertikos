32a3ee445a3b589537c8f669879275a1ebc1713e
Tiger Wang <tiger.wangj@gmail.com>
2022-10-28 20:54:27 -0400

    user

--- user/pingpong/ping.c
+++ user/pingpong/ping.c
@@ -7,15 +7,20 @@ int main(int argc, char **argv)
     unsigned int i;
     printf("ping started.\n");
 
-    // fast producing
-    for (i = 0; i < 10; i++)
-        produce(i);
+    produce(5);
+    produce(10);
+    produce(7);
+    produce(11);
 
-    // slow producing
-    for (i = 0; i < 40; i++) {
-        if (i % 4 == 0)
-            produce(i);
-    }
+//    // fast producing
+//    for (i = 0; i < 10; i++)
+//        produce(i);
+//
+//    // slow producing
+//    for (i = 0; i < 40; i++) {
+//        if (i % 4 == 0)
+//            produce(i);
+//    }
 
     return 0;
 }
