81aba75fbf534f7057adff7b10c1d858221265c8
Tiger Wang <tiger.wangj@gmail.com>
2022-10-31 19:53:45 -0400

    remove intr

--- kern/lib/cv.c
+++ kern/lib/cv.c
@@ -16,9 +16,9 @@ void cv_wait(cv_t *cv, spinlock_t *lk) {
     cv->waiting[cv->next_empty % NUM_IDS] = cur_pid;
     cv->next_empty++;
 
-    intr_local_disable();
+//    intr_local_disable();
     KERN_DEBUG("Added process %d to CV waiting queue\n", cur_pid);
-    intr_local_enable();
+//    intr_local_enable();
 
     spinlock_release(lk);
     thread_suspend();
@@ -30,9 +30,9 @@ void cv_signal(cv_t *cv) {
         unsigned int wake_pid = cv->waiting[cv->front % NUM_IDS];
         cv->front++;
 
-        intr_local_disable();
+//        intr_local_disable();
         KERN_DEBUG("Signaling process %d to be ready\n", wake_pid);
-        intr_local_enable();
+//        intr_local_enable();
 
         thread_make_ready(wake_pid);
     }
@@ -43,9 +43,9 @@ void cv_broadcast(cv_t *cv) {
         unsigned int wake_pid = cv->waiting[cv->front % NUM_IDS];
         cv->front++;
 
-        intr_local_disable();
+//        intr_local_disable();
         KERN_DEBUG("Broadcasting process %d to be ready\n", wake_pid);
-        intr_local_enable();
+//        intr_local_enable();
 
         thread_make_ready(wake_pid);
     }
